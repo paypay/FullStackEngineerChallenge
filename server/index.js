663558b529cb173887e4c4150adeabf8c25e1cfe
Abhijit <abhi.floyd@gmail.com>
2019-06-10 23:25:02 +0900

    Architecture

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const keys = require('./config/keys')
+require('./models/user')
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
