ed36f0730f31fd208e86443c50101b2d3349af08
houko <kohou@ctw-inc.net>
2020-11-11 15:45:39 +0900

    fix pagination

--- employee-server/src/main/java/jp/net/paypay/employee/config/MybatisPlusConfig.java
+++ employee-server/src/main/java/jp/net/paypay/employee/config/MybatisPlusConfig.java
@@ -1,16 +1,21 @@
 package jp.net.paypay.employee.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(value = {PaginationInnerInterceptor.class})
 @Slf4j
 public class MybatisPlusConfig {
-
-    @Bean
-    public MybatisPlusInterceptor paginationInterceptor() {
-        return new MybatisPlusInterceptor();
-    }
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    return interceptor;
+  }
 }
