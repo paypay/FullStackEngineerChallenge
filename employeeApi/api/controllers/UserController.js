91c98de388baf26f480a610229b47eb948c4838b
Vishnu S Babu <vsb.creative@gmail.com>
2021-05-22 20:09:26 +0530

    API: getEmployees

--- employeeApi/api/controllers/UserController.js
+++ employeeApi/api/controllers/UserController.js
@@ -15,7 +15,6 @@ module.exports = {
         console.log(req.allParams());
         var username    =   req.param('username');
         var password    =   req.param('password');
-        var userType    =   req.param('user_type');
         // var tokenExpiry =   req.options.settingsKeyValue.TOKEN_EXPIRY_HOUR;
 		if(!username){
             return res.json(200, {status: 2, status_type: 'Failure' , message: 'Please pass username'});
@@ -43,9 +42,30 @@ module.exports = {
 				}
 			});
 			  
-		}
-		
-   }
+		}	
+   },
+   getEmployees: function(req, res){
+    var token;
+    console.log("Get Emp Controller");
+    console.log(req.allParams());
+        var conQuery    =   "userType = 'user'";
+        var query       =   "SELECT * from user where "+conQuery;
+        User.getDatastore().sendNativeQuery(query, function(err, results) {
+            if (err) {
+                
+                return res.status(200).json({status: 2, status_type: 'Failure' ,message: 'Some error in selecting user', error_details: err});
+            }else{
+                var jsonResult  =   results.rows;
+                if(jsonResult.length){
+                    return res.status(200).json({status: 1, status_type: 'Success', data :   jsonResult });
+                } else {
+                    return res.status(200).json({status: 1, status_type: 'Success' , data :   [] });
+
+                }
+            }
+        });
+	
+}
 
 };
 
