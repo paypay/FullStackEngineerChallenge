99973890d5ad0c3eb0aec1001ef5e639ced885a3
Vishnu S Babu <vsb.creative@gmail.com>
2021-05-23 20:56:55 +0530

    CLIENT: Initial commit and login component

---
+++ employeeAdmin/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+    { path: '', component: LoginComponent},
+    { path: 'login', component: LoginComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
